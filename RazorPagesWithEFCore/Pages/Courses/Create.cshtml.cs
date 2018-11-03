7ef3b6adf1f7097ce35e2a7e5e6e53f6de7cac0a
Monirul Islam <mohammadmonirul@gmail.com>
2018-11-03 23:37:55 +0100

    Add project files.

---
+++ RazorPagesWithEFCore/Pages/Courses/Create.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPagesWithEFCore.Models;
+
+namespace RazorPagesWithEFCore.Pages.Courses
+{
+    public class CreateModel : DepartmentNamePageModel
+    {
+        private readonly SchoolContext _context;
+
+        public CreateModel(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            PopulateDepartmentsDropDownList(_context);
+
+            return Page();
+        }
+
+        [BindProperty]
+        public Course Course { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var emptyCourse = new Course();
+
+            if (await TryUpdateModelAsync<Course>(
+                emptyCourse,
+                "course",
+                s => s.CourseID, s => s.DepartmentID, s => s.Title, s => s.Credits
+                ))
+            {
+                _context.Courses.Add(emptyCourse);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+
+            // Select DepartmentID if TryUpdateModelAsync fails.
+            PopulateDepartmentsDropDownList(_context, emptyCourse.DepartmentID);
+            return Page();
+        }
+    }
+}
